2f6a5e8eed56bff05d746fe0833dff963ca15697
Aryan <aryanpandey5315@gmail.com>
2023-02-12 15:34:21 +0530

    Taller problem

---
+++ TALLER.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        if(a>b){
+            cout<<"A\n";
+        } else{
+            cout<<"B\n";
+        }
+    }
+	return 0;
+}
