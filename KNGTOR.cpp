f51f92041a648702993f24a702e75234e7cdd2ab
Aryan <aryanpandey5315@gmail.com>
2023-02-13 14:23:28 +0530

    king of tour problem

---
+++ KNGTOR.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int m,n;
+	    cin>>m>>n;
+	    cout<<(m*5)+(n*7)<<endl; 
+	}
+	return 0;
+}
