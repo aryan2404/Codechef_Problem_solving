ab8610f4338a418d6f620e08ad38cbb8c34f9e6d
Aryan <aryanpandey5315@gmail.com>
2023-03-05 16:50:52 +0530

    this is atm problem

---
+++ atm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t; 
+    while(t--){
+        int a;
+        cin>>a;
+        cout<<a*60/20<<endl;
+        }
+	return 0;
+}
