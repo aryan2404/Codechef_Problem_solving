91e9376faf32156320e06f7dbd2863a42c0589e1
Aryan <aryanpandey5315@gmail.com>
2023-03-11 20:13:14 +0530

    This is one more problem

---
+++ onemore.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	      if(x<=24){
+	        cout<<"NO"<<endl;
+	    }
+	    else{
+	        cout<<"YES"<<endl;
+	    }
+	}
+	return 0;
+}
