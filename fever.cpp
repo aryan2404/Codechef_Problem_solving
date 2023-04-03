4eaf1c092b5e414e8ce0832f131b8c4ff0dd2620
Aryan <aryanpandey5315@gmail.com>
2023-04-04 00:39:49 +0530

    this is about fever

---
+++ fever.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>98)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	    
+	}
+	return 0;
+}
