6bab77972cb494eb3fb09dd9ae34f82c43a25600
Aryan <aryanpandey5315@gmail.com>
2023-04-04 00:45:42 +0530

    slepp depration

---
+++ sleep.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x<7)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	}
+	return 0;
+}
