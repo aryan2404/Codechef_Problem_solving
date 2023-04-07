c2723f216ca3a4ccf681c9e17ef672b1c241fd4f
Aryan <aryanpandey5315@gmail.com>
2023-04-07 14:04:29 +0530

    07/04/2023

---
+++ apple.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	  int x,y;
+	    cin>>x>>y;
+	    if(t>=x+y)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	return 0;
+}
