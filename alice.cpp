b342d238161a7976715dc069c0d3625d210216b8
Aryan <aryanpandey5315@gmail.com>
2023-04-09 23:51:47 +0530

    alice and bob marks

---
+++ alice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	if(x>=2*y)
+	cout<<"yes"<<endl;
+	else
+	cout<<"no"<<endl;
+	return 0;
+}
