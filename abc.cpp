27dd0631f4e840952c0a482135ad63561bb50b89
Aryan <aryanpandey5315@gmail.com>
2023-04-11 23:48:49 +0530

    problem

---
+++ abc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() { int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x;
+        cin>>y;
+        if(x>=y){
+            cout<<7-x<<endl;
+        }
+        else{
+            cout<<7-y<<endl;}
+        }
+	// your code goes here
+	return 0;
+}
