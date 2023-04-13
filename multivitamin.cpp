4210a2e7914947f9e1553e2b9c5e0cc16677322e
Aryan <aryanpandey5315@gmail.com>
2023-04-13 19:45:24 +0530

    multivitamin

---
+++ multivitamin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if((x*3)>y)
+	    cout<<"no"<<endl;
+	    else
+	    cout<<"yes"<<endl;
+	}
+	return 0;
+}
